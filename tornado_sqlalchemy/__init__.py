5c315ec20205d65ba0d93043f73e72e4efaf0687
Siddhant Goel <siddhantgoel@gmail.com>
2017-08-07 13:04:00 +0200

    update docstring

--- tornado_sqlalchemy/__init__.py
+++ tornado_sqlalchemy/__init__.py
@@ -52,6 +52,7 @@ class SessionFactory(object):
 
     :param database_url: Database URL
     :param pool_size: Connection pool size
+    :param use_native_unicode: Enable/Disable native unicode support
     :param engine_events: List of (name, listener_function) tuples to subscribe
     to engine events
     :param session_events: List of (name, listener_function) tuples to

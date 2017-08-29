fc907b63a907598caaadace7bc9e4b46c1caab54
Siddhant Goel <siddhantgoel@gmail.com>
2017-08-29 16:09:16 +0200

    add publish command to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import codecs
 import os
-from sys import version_info
+import sys
+from shutil import rmtree
 
-from setuptools import setup
+from setuptools import setup, Command
 
 
 pwd = os.path.abspath(os.path.dirname(__file__))
@@ -18,15 +19,53 @@ def install_requires():
         'SQLAlchemy >= 1.0'
     ]
 
-    if version_info.major == 2:
+    if sys.version_info.major == 2:
         deps.append('futures >= 3.0.0')
 
     return deps
 
 
+class PublishCommand(Command):
+    """Support setup.py publish.
+
+    https://github.com/kennethreitz/setup.py/blob/master/setup.py
+    """
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.sep.join(('.', 'dist')))
+        except FileNotFoundError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system(
+            '{0} setup.py sdist bdist_wheel --universal'.format(
+                sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
 setup(
     name='tornado_sqlalchemy',
-    version='0.2.2',
+    version='0.3.0',
     description='SQLAlchemy helpers for working in Tornado',
     long_description=long_description,
     author='Siddhant Goel',

1e64074004c58e118e85f421982e9d7855473ec1
Siddhant Goel <siddhantgoel@gmail.com>
2018-03-28 21:13:37 +0200

    bump version

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ class PublishCommand(Command):
 
 setup(
     name='tornado_sqlalchemy',
-    version='0.3.3',
+    version='0.4.0',
     description='SQLAlchemy helpers for working in Tornado',
     long_description=long_description,
     author='Siddhant Goel',

525807004e5c90db876a54a5553a8f7730904a84
Siddhant Goel <me@sgoel.org>
2018-09-13 07:49:48 +0200

    update tornado usage example

--- examples/tornado_web.py
+++ examples/tornado_web.py
@@ -1,7 +1,7 @@
 from sqlalchemy import BigInteger, Column, String
 from tornado.gen import coroutine
 from tornado.ioloop import IOLoop
-from tornado.options import options
+from tornado.options import define, options, parse_command_line
 from tornado.web import Application, RequestHandler
 
 from tornado_sqlalchemy import (SessionMixin, as_future, declarative_base,
@@ -11,6 +11,9 @@ from tornado_sqlalchemy import (SessionMixin, as_future, declarative_base,
 DeclarativeBase = declarative_base()
 
 
+define('database-url', type=str, help='Database URL')
+
+
 class User(DeclarativeBase):
     __tablename__ = 'users'
 
@@ -18,15 +21,17 @@ class User(DeclarativeBase):
     username = Column(String(255), unique=True)
 
 
-class SyncWebRequestHandler(SessionMixin, RequestHandler):
+class SynchronousRequestHandler(SessionMixin, RequestHandler):
     def get(self):
         with self.make_session() as session:
             count = session.query(User).count()
 
+        # OR count = self.session.query(User).count()
+
         self.write('{} users so far!'.format(count))
 
 
-class AsyncWebRequestHandler(SessionMixin, RequestHandler):
+class GenCoroutinesRequestHandler(SessionMixin, RequestHandler):
     @coroutine
     def get(self):
         with self.make_session() as session:
@@ -35,21 +40,27 @@ class AsyncWebRequestHandler(SessionMixin, RequestHandler):
         self.write('{} users so far!'.format(count))
 
 
-class UsesSelfSessionRequestHandler(SessionMixin, RequestHandler):
-    @coroutine
-    def get(self):
-        count = self.session.query(User).count()
+class NativeCoroutinesRequestHandler(SessionMixin, RequestHandler):
+    async def get(self):
+        with self.make_session() as session:
+            count = await as_future(session.query(User).count)
 
         self.write('{} users so far!'.format(count))
 
 
 if __name__ == '__main__':
+    parse_command_line()
+
+    assert options.database_url, "Need a database URL"
+
     session_factory = make_session_factory(options.database_url)
 
     Application([
-        (r'/sync', SyncWebRequestHandler)
-        (r'/async', AsyncWebRequestHandler)
-        (r'/uses-self-session', UsesSelfSessionRequestHandler)
+        (r'/sync', SynchronousRequestHandler),
+        (r'/gen-coroutines', GenCoroutinesRequestHandler),
+        (r'/native-coroutines', NativeCoroutinesRequestHandler),
     ], session_factory=session_factory).listen(8888)
+
+    print('Listening on port 8888')
 
     IOLoop.current().start()

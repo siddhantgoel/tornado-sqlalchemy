0b2d85f8ac567455179d85aaca8bff0850ddb428
Siddhant Goel <me@sgoel.org>
2019-12-29 13:45:53 +0100

    add request handler test cases for multiple databases

--- tests/test_multiple_databases.py
+++ tests/test_multiple_databases.py
@@ -1,5 +1,11 @@
+from http import HTTPStatus
 from unittest import TestCase, mock
+
 from sqlalchemy import Column, BigInteger, String
+from tornado.testing import AsyncHTTPTestCase
+from tornado.web import Application, RequestHandler
+
+from tornado_sqlalchemy import SessionMixin
 
 from ._common import db, mysql_url, mysql_url_1, mysql_url_2
 
@@ -69,3 +75,50 @@ class MultipleDatabasesTestCase(TestCase):
 
         self.assertEqual(foo_count, 1)
         self.assertEqual(bar_count, 2)
+
+
+class RequestHandlersTestCase(AsyncHTTPTestCase, TestCase):
+    def __init__(self, *args, **kwargs):
+        super(RequestHandlersTestCase, self).__init__(*args, **kwargs)
+
+        db.configure(
+            uri=mysql_url, binds={'foo': mysql_url_1, 'bar': mysql_url_2}
+        )
+
+        class IndexRequestHandler(SessionMixin, RequestHandler):
+            def get(h_self):
+                with h_self.make_session() as session:
+                    session.add(Foo('first-foo'))
+
+                    session.add(Bar('first-bar'))
+                    session.add(Bar('second-bar'))
+
+                    session.flush()
+
+                    foo_count = session.query(Foo).count()
+                    bar_count = session.query(Bar).count()
+
+                h_self.write('{}, {}'.format(foo_count, bar_count))
+
+        handlers = ((r'/', IndexRequestHandler),)
+
+        self._application = Application(handlers, db=db)
+
+    def setUp(self, *args, **kwargs):
+        super(RequestHandlersTestCase, self).setUp(*args, **kwargs)
+
+        db.create_all()
+
+    def tearDown(self, *args, **kwargs):
+        db.drop_all()
+
+        super(RequestHandlersTestCase, self).tearDown(*args, **kwargs)
+
+    def get_app(self):
+        return self._application
+
+    def test_count(self):
+        response = self.fetch('/', method='GET')
+
+        self.assertEqual(response.code, HTTPStatus.OK.value)
+        self.assertEqual(response.body.decode('utf-8'), '1, 2')

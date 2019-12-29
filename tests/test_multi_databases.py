544677aca93da966620fb3a0d71fe418329c3fac
dingyaguang <dingyaguang@itiger.com>
2019-12-29 16:17:01 +0800

    add bind key test; add same table name test

--- tests/test_multi_databases.py
+++ tests/test_multi_databases.py
@@ -1,10 +1,7 @@
 from unittest import TestCase, mock
 from sqlalchemy import Column, BigInteger, String
 
-from ._common import db, User, mysql_url, mysql_url_1, mysql_url_2
-
-
-db.Model2 = db.make_declarative_base()
+from ._common import db, mysql_url, mysql_url_1, mysql_url_2
 
 
 class Foo(db.Model):
@@ -18,26 +15,15 @@ class Foo(db.Model):
         self.foo = foo
 
 
-class UserInBar(db.Model2):
+class Bar(db.Model):
     __bind_key__ = 'bar'
-    __tablename__ = 'users'
+    __tablename__ = 'bar'
 
     id = Column(BigInteger, primary_key=True)
-    username = Column(String(64), unique=True)
+    bar = Column(String(64))
 
-    def __init__(self, username):
-        self.username = username
-
-
-class FooInBar(db.Model2):
-    __bind_key__ = 'bar'
-    __tablename__ = 'foo'
-
-    id = Column(BigInteger, primary_key=True)
-    foo = Column(String(64))
-
-    def __init__(self, foo):
-        self.foo = foo
+    def __init__(self, bar):
+        self.bar = bar
 
 
 class MultiDatabasesTestCase(TestCase):
@@ -52,48 +38,31 @@ class MultiDatabasesTestCase(TestCase):
         self._application.settings = {'db': db}
 
         db.create_all()
-        db.Model2.metadata.create_all(db.get_engine('bar'))
 
     def tearDown(self, *args, **kwargs):
         db.drop_all()
-        db.Model2.metadata.drop_all(db.get_engine('bar'))
-
         super(MultiDatabasesTestCase, self).tearDown(*args, **kwargs)
 
     def test_add_objects(self):
         session = db.sessionmaker()
 
-        session.add(User('a'))
-        session.add(User('b'))
+        session.add(Foo('foo'))
         session.commit()
-        user_count = session.query(User).count()
+        foo_count = (
+            db.get_engine('foo')
+            .execute("SELECT COUNT(*) FROM foo")
+            .fetchone()[0]
+        )
 
-        foo = Foo('b')
-        session.add(foo)
+        session.add(Bar('first bar'))
+        session.add(Bar('second bar'))
         session.commit()
-        foo_count = session.query(Foo).count()
-
+        bar_count = (
+            db.get_engine('bar')
+            .execute("SELECT COUNT(*) FROM bar")
+            .fetchone()[0]
+        )
         session.close()
 
-        assert user_count == 2
         assert foo_count == 1
-
-    def test_multiple_bases(self):
-        session = db.sessionmaker()
-
-        session.add(User('a'))
-        session.add(User('b'))
-        session.commit()
-        user_count_1 = session.query(User).count()
-
-        session.add(UserInBar('a'))
-        session.commit()
-
-        user_in_bar_count = session.query(UserInBar).count()
-        user_count_2 = session.query(User).count()
-
-        session.close()
-
-        assert user_count_1 == 2
-        assert user_in_bar_count == 1
-        assert user_count_2 == 2
+        assert bar_count == 2
---
+++ tests/test_same_tablename.py
@@ -0,0 +1,57 @@
+from unittest import TestCase, mock
+from sqlalchemy import Column, BigInteger, String
+
+from ._common import db, User, mysql_url, mysql_url_2
+
+
+db.Model2 = db.make_declarative_base()
+
+
+class UserInBar(db.Model2):
+    __bind_key__ = 'bar'
+    __tablename__ = 'users'
+
+    id = Column(BigInteger, primary_key=True)
+    username = Column(String(64), unique=True)
+
+    def __init__(self, username):
+        self.username = username
+
+
+class MultiDatabasesTestCase(TestCase):
+    def setUp(self, *args, **kwargs):
+        super(MultiDatabasesTestCase, self).setUp(*args, **kwargs)
+
+        db.configure(uri=mysql_url, binds={'bar': mysql_url_2})
+
+        self._application = mock.Mock()
+        self._application.settings = {'db': db}
+
+        db.create_all()
+        db.Model2.metadata.create_all(db.get_engine('bar'))
+
+    def tearDown(self, *args, **kwargs):
+        db.drop_all()
+        db.Model2.metadata.drop_all(db.get_engine('bar'))
+
+        super(MultiDatabasesTestCase, self).tearDown(*args, **kwargs)
+
+    def test_multiple_bases(self):
+        session = db.sessionmaker()
+
+        session.add(User('a'))
+        session.add(User('b'))
+        session.commit()
+        user_count_1 = session.query(User).count()
+
+        session.add(UserInBar('a'))
+        session.commit()
+
+        user_in_bar_count = session.query(UserInBar).count()
+        user_count_2 = session.query(User).count()
+
+        session.close()
+
+        assert user_count_1 == 2
+        assert user_in_bar_count == 1
+        assert user_count_2 == 2

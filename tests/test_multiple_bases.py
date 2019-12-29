99bfd3a33f748e3fa300de965a314b1fad9a67fb
Siddhant Goel <me@sgoel.org>
2019-12-29 13:24:05 +0100

    extend tests for multiple declarative bases

--- tests/test_multiple_bases.py
+++ tests/test_multiple_bases.py
@@ -1,13 +1,26 @@
 from unittest import TestCase, mock
 from sqlalchemy import Column, BigInteger, String
 
-from ._common import db, User, mysql_url, mysql_url_2
+from ._common import db, User, mysql_url, mysql_url_1, mysql_url_2
 
 
-db.Model2 = db.make_declarative_base()
+db.FooModel = db.make_declarative_base()
+
+db.BarModel = db.make_declarative_base()
 
 
-class UserInBar(db.Model2):
+class FooUser(db.FooModel):
+    __bind_key__ = 'foo'
+    __tablename__ = 'users'
+
+    id = Column(BigInteger, primary_key=True)
+    username = Column(String(64), unique=True)
+
+    def __init__(self, username):
+        self.username = username
+
+
+class BarUser(db.BarModel):
     __bind_key__ = 'bar'
     __tablename__ = 'users'
 
@@ -22,36 +35,45 @@ class MultipleBasesTestCase(TestCase):
     def setUp(self, *args, **kwargs):
         super(MultipleBasesTestCase, self).setUp(*args, **kwargs)
 
-        db.configure(uri=mysql_url, binds={'bar': mysql_url_2})
+        db.configure(
+            uri=mysql_url, binds={'foo': mysql_url_1, 'bar': mysql_url_2}
+        )
 
         self._application = mock.Mock()
         self._application.settings = {'db': db}
 
         db.create_all()
-        db.Model2.metadata.create_all(db.get_engine('bar'))
+        db.FooModel.metadata.create_all(db.get_engine('foo'))
+        db.BarModel.metadata.create_all(db.get_engine('bar'))
 
     def tearDown(self, *args, **kwargs):
         db.drop_all()
-        db.Model2.metadata.drop_all(db.get_engine('bar'))
+        db.FooModel.metadata.drop_all(db.get_engine('foo'))
+        db.BarModel.metadata.drop_all(db.get_engine('bar'))
 
         super(MultipleBasesTestCase, self).tearDown(*args, **kwargs)
 
     def test_multiple_bases(self):
         session = db.sessionmaker()
 
-        session.add(User('a'))
-        session.add(User('b'))
-        session.commit()
-        user_count_1 = session.query(User).count()
-
-        session.add(UserInBar('a'))
+        session.add(User('first'))
         session.commit()
 
-        user_in_bar_count = session.query(UserInBar).count()
-        user_count_2 = session.query(User).count()
+        session.add(FooUser('first'))
+        session.add(FooUser('second'))
+        session.commit()
+
+        session.add(BarUser('first'))
+        session.add(BarUser('second'))
+        session.add(BarUser('third'))
+        session.commit()
+
+        user_count = session.query(User).count()
+        foo_user_count = session.query(FooUser).count()
+        bar_user_count = session.query(BarUser).count()
 
         session.close()
 
-        self.assertEqual(user_count_1, 2)
-        self.assertEqual(user_in_bar_count, 1)
-        self.assertEqual(user_count_2, 2)
+        self.assertEqual(user_count, 1)
+        self.assertEqual(foo_user_count, 2)
+        self.assertEqual(bar_user_count, 3)
